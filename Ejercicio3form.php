cdc1718515bb43c0e52b06d01077ccf18afabdcb
LiZziGandarillas <71907257+LiZziGandarillas@users.noreply.github.com>
2021-04-23 19:53:37 -0400

    Update Ejercicio3form.php

--- Ejercicio3form.php
+++ Ejercicio3form.php
@@ -6,6 +6,15 @@
 	<input type="submit" name="Enviar"/>
 	<br/> <br/> <br/>
 	<input type="hidden" name="phptime">
+	<?php 
+		$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		try{
+			$consulta = "insert into registro (nombres,apellidos,fechaHora) values ('"nombres,apellidos,phptime"');" ;
+		    $conexion->query($consulta);
+		  } catch (Exception $e) {
+		    die ("Se produjo un error");
+		  }
+	?>
 </form>
 <script type="text/javascript">
 	var d = new Date().toLocaleDateString().concat(" ", new Date().getHours(), ":", new Date().getMinutes(), ":", new Date().getSeconds());
